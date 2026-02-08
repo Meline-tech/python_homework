1f3101a2741e9889a8cd06a34c9ac1bf13c8df39
Meline <melgevorgyan1999@gmail.com>
2026-02-08 18:38:02 +0400

    Statistical presentation of the data in column 6 of the file

--- utils/generic.py
+++ utils/generic.py
@@ -8,3 +8,14 @@ import pandas as pd
 def read_file_with_pandas(filename):
   df=pd.read_csv(filename)
   return df
+def column_values(df, column_name):
+    return df[column_name]
+file_name="/content/sample_data/mnist_train_small.csv"
+col_name="6"
+df=read_file_with_pandas(file_name)
+col_data=column_values(df, col_name)
+maximum=min_max(col_data)
+minimum=min_max(col_data)
+average=array_mean(col_data)
+print (f"max={maximum} , min={minimum}, mean={average}" )
+
